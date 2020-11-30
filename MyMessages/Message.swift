8ce261040fc3e5ca0f64e9511ed25d1956d5108b
Matthew Robertson <marobert@microsoft.com>
2020-11-29 17:15:33 -0800

    Initial commit

---
+++ MyMessages/Message.swift
@@ -0,0 +1,36 @@
+//
+//  Message.swift
+//  MyMessages
+//
+//  Created by marobert on 2020-11-30.
+//
+
+import Foundation
+import MessageKit
+import AzureCommunicationChat
+
+public struct Message: MessageType {
+
+    /// The unique identifier for the message.
+    public var messageId: String
+
+    /// The date the message was sent.
+    public var sentDate: Date = Date()
+    
+    /// The sender of the message.
+    public var sender: SenderType
+
+    /// The kind of message and its underlying kind.
+    public var kind: MessageKind
+}
+
+extension Message {
+    init(acsMessage: ChatMessage) {
+        self.init(
+            messageId: acsMessage.id!,
+            sentDate: acsMessage.createdOn ?? Date(),
+            sender: UserDirectory.getSender(fromId: acsMessage.senderId!),
+            kind: .text(acsMessage.content ?? "")
+        )
+    }
+}
